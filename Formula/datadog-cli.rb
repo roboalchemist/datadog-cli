18060a3d61c5c05f0f6fb52cb2b0f287cac671cb
Joe Schlesinger <157418651+joe-verkada@users.noreply.github.com>
2026-02-25 20:40:13 -0800

    DDCLI-19: release automation with Gitea CI and homebrew-private formula

---
+++ Formula/datadog-cli.rb
@@ -0,0 +1,18 @@
+class DatadogCli < Formula
+  desc "Read-only CLI for querying Datadog APIs"
+  homepage "https://github.com/roboalchemist/datadog-cli"
+  url "https://github.com/roboalchemist/datadog-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match "datadog-cli version", shell_output("#{bin}/datadog-cli --version")
+  end
+end
